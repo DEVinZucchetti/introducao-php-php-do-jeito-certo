ad1002bff07d98e23e5818ef20621235192a3e2a
TaisaMonteiro <taisa_monteiro@estudante.sesisenai.org.br>
2023-11-08 09:35:42 -0300

    Finalização do exercício 1

---
+++ TaisaMonteiro/pages/calculadora.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calculadora</title>
+</head>
+
+<body>
+
+  <form method="post" action="resultado.php">
+    
+    <label>Qual a tabuada que você deseja calcular?</label> <br/>
+    <input type="number" name="valor"/> <br/>
+
+    <button type="submit">Calcular</button>
+  </form>
+
+</body>
+
+</html>
---
+++ TaisaMonteiro/pages/lanchonete.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lanchonete</title>
+</head>
+
+<body>
+
+  <form method="post" action="calcular_preco.php">
+
+    <label>Código</label><br />
+    <input type="text" name="codigo" /><br />
+
+    <label>Quantidade</label><br />
+    <input type="number" name="quantidade" /><br />
+
+    <button type="submit">Calcular preço</button>
+  </form>
+
+    <h3>Código: 3456 - Produto: Coxinha</h3>
+    <h3>Código: 3457 - Produto: Sushi</h3>
+    <h3>Código: 3458 - Produto: Pastel</h3>
+  
+</body>
+
+</html>
