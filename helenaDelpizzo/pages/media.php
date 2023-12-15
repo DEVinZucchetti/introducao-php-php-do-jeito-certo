6f676de304eb8453ef9da0291a34259a5a7300e6
Helena Delpizzo <hdelpizzo@gmail.com>
2023-12-14 22:17:37 -0300

    feat: calcular média

--- helenaDelpizzo/pages/media.php
+++ helenaDelpizzo/pages/media.php
@@ -3,9 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Média</title>
 </head>
 <body>
-  media
+  <form method="post" action="calcular_media.php">
+    <label>Nota 1</label><br />
+    <input type="number" name="nota1" /><br />
+    <label>Nota 2</label><br />
+    <input type="number" name="nota2" /><br />
+    <label>Nota 3</label><br />
+    <input type="number" name="nota3" /><br />
+    <label>Nota 4</label><br />
+    <input type="number" name="nota4" /><br />
+    <button type="submit">Calcular média</button>
+  </form>
 </body>
 </html>
